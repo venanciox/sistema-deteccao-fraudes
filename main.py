3fd21f3f7df02b4878469100af22d0b38ea57f18
Caio <caiovenancio.paiva@gmail.com>
2026-08-17 19:17:28 -0300

    V0.2 - Organização com Funções

--- main.py
+++ main.py
@@ -1,38 +1,60 @@
-print("=== SISTEMA DE DETECÇÃO DE FRAUDES ===")
+def calcular_risco(valor, hora, novo_destinatario):
+    risco = 0
+    motivos = []
 
-valor = float(input("Digite o valor da transferência: "))
-hora = int(input("Digite a hora da transferência: "))
-novo_destinatario = input("Destinatário novo? (s/n): ")
+    if valor > 4999:
+        risco += 30
+        motivos.append("Valor alto")
 
-risco = 0
-motivos = []
+    if 0 <= hora <= 5:
+        risco += 25
+        motivos.append("Horário incomum")
 
-if valor > 4999:
-    risco += 30
-    motivos.append("Valor alto")
+    if novo_destinatario.lower() == "s":
+        risco += 20
+        motivos.append("Destinatário novo")
 
-if 0 <= hora <= 5:
-    risco += 25
-    motivos.append("Horário incomum")
+    return risco, motivos
 
-if novo_destinatario.lower() == "s":
-    risco += 20
-    motivos.append("Destinatário novo")
+def classificar_risco(risco):
+    if risco >= 60:
+        return "🔴 ALTO RISCO"
+    elif risco >= 30:
+        return "🟡 RISCO MÉDIO"
+    else:
+        return "🟢 BAIXO RISCO"
 
-print()
-print(f"Risco: {risco}%")
+def mostrar_resultado(risco, classificacao, motivos):
+    print()
+    print(f"Risco: {risco}%")
+    print(classificacao)
 
-if risco >= 60:
-    print("🔴 ALTO RISCO")
+    print()
+    print("Motivos: ")
 
-elif risco >= 30:
-    print("🟡 RISCO MÉDIO")
+    for motivo in motivos:
+        print(f"- {motivo}")
 
-else:
-    print("🟢 BAIXO RISCO")
+def main():
+    print("=== SISTEMA DE DETECÇÃO DE FRAUDES ===")
 
-print()
-print("Motivos: ")
+    valor = float(input("Digite o valor da transferência: "))
+    hora = int(input("Digite a hora da transferência: "))
+    novo_destinatario = input("Destinatário novo? (s/n): ")
 
-for motivo in motivos:
-    print(f"- {motivo}")
+    risco, motivos = calcular_risco(
+        valor,
+        hora,
+        novo_destinatario
+    )
+
+    classificacao = classificar_risco(risco)
+
+    mostrar_resultado(
+        risco,
+        classificacao,
+        motivos
+    )
+
+if __name__ == "__main__":
+    main()
