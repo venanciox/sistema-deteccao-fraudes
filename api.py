4734f1ddaf2f66bd242c26e9a602a0744de75850
Caio <caiovenancio.paiva@gmail.com>
2026-08-27 21:57:50 -0300

    V1.5 - Criação do schema Pydantic e rota POST para receber transações

--- api.py
+++ api.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI(title="Motor Antifraude API")
+
+class Transacao(BaseModel):
+    valor: float
+    hora: int
+    novo_destinatario: str
+    transacoes_ultima_hora: int
 
 @app.get("/")
 def raiz():
     return {
         "sistema": "Motor Antifraude",
         "status": "Online",
-        "versao": "1.4"
+        "versao": "1.5"
+    }
+
+@app.post("/analisar")
+def analisar_transacao(transacao: Transacao):
+    return {
+        "mensagem": "Transação recebida com sucesso na Web!",
+        "dados_recebidos": transacao
     }
